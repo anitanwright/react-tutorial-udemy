b0f9eef4f11cf8a43c99910bac1a0d028f16c140
Anita Wright <anitanwright@gmail.com>
2021-11-24 14:31:12 -0700

    JSX Rules

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDom from 'react-dom'
 
-// stateless functional component
-// always return JSX
+//JSX RULES
+//return single element
+// use div, section, article, or Fragment
+// use camelCase for HTML attribute
+// className instead of class
+// close every element
+// formatting
 
 function Greeting() {
-  return <h4>This is Anita and this is my first component</h4>
+  return (
+    <section>
+      <div>
+        <h2>Hello how are you today?</h2>
+        <ul>
+          <li>
+            <a href='#'>hello world</a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 const Hello = () => {
@@ -17,8 +34,8 @@ const Hello = () => {
   )
 }
 
-ReactDom.render(<Hello />, document.getElementById('root'))
-
 // comment out one of the renders to see the change in the browser
+
+ReactDom.render(<Hello />, document.getElementById('root'))
 
 ReactDom.render(<Greeting />, document.getElementById('root'))
