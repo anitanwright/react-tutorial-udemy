0365e29e8bed443dbfca26bd097a03c27d4f8dc7
Anita Wright <anitanwright@gmail.com>
2021-11-24 15:42:07 -0700

    Book List example

--- src/index.js
+++ src/index.js
@@ -50,8 +50,42 @@ const Person = () => <h2>anita wright</h2>
 const Message = () => {
   return <p>this is my message</p>
 }
+
+//Book List mini-project for nested components
+// renders Book component which includes the additional 3 nested within
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  )
+}
+
+// Book component has Image, Title, and Author components nested within.
+const Book = () => {
+  return (
+    <article>
+      {' '}
+      <Title />
+      by <Author />
+      <Image />
+    </article>
+  )
+}
+const Image = () => (
+  <img
+    src='https://images-na.ssl-images-amazon.com/images/I/51Ua3JTN8iS._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+    eslint-disable-next-line
+    no-undef
+    alt='Diary of a Wimpy Kid'
+  />
+)
+const Title = () => <h3>Diary of a Wimpy Kid</h3>
+const Author = () => <h3>Sum Dood</h3>
+
 // comment out the renders that don't go with the function chosen above to see the result in the browser.
 
 //ReactDom.render(<Hello />, document.getElementById('root'))
-ReactDom.render(<Saying />, document.getElementById('root'))
+//ReactDom.render(<Saying />, document.getElementById('root'))
 //ReactDom.render(<Greeting />, document.getElementById('root'))
+ReactDom.render(<BookList />, document.getElementById('root'))
